a14b3b9fa956435399c2457214d16909f881a033
Alexandre Santos <alexandre.gomes@innovasolucoes.com.br>
2022-05-09 21:08:19 -0300

    Dia 02 Finalizado

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,17 @@
+import { Warning } from 'phosphor-react';
 import { Widget } from './components/Widget'
 
 function App() {
-  return <Widget/>
+  return (
+    <>
+      <section className='bg-[#FF6464] font-bold p-5 ml-auto mr-auto mt-5 rounded justify-center md:w-[30%] sm:w-[100%]'>
+        <h1 className='text-[#630606] flex items-start group'><Warning size={25} /> Ops!</h1>
+        <h4>Ocorreu um erro, favor enviar um feedback.</h4>
+        <h4 className='font-normal'>Código do erro: SYS-96522.</h4>
+      </section>
+      <Widget/>
+    </>
+  );
 }
 
 export default App
--- web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -1,5 +1,29 @@
-export function FeedbackSuccessStep() {
+import { CheckSquare } from "phosphor-react";
+import { CloseButton } from "../../CloseButton";
+
+interface FeedbackSuccessStepsProps {
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({ onFeedbackRestartRequested }: FeedbackSuccessStepsProps) {
     return (
-        ''
+        <>
+            <header>
+                <CloseButton />
+            </header>
+
+            <div className="flex flex-col items-center py-10 w-[304px]">
+                <CheckSquare size={38} weight="duotone" className="text-[#008000]" />
+
+                <span className="text-xl mt-2">Agradeçemos o Feedback!</span>
+
+                <button 
+                    onClick={onFeedbackRestartRequested}
+                    type="button"
+                    className="py-2 px-6 mt-6 bg-brand-500 rounded-md border-transparent text-sm leading-6 hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-brand-900 focus:ring-brand-500 transition-colors">
+                    Quero enviar outro.
+                </button>
+            </div>
+        </>
     );
 }
